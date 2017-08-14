23a5989ddd90edd87e44fd92e7603f52ce3d2697
coleworsley <cole.s.worsley@gmail.com>
2017-08-13 18:00:43 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const urlLogger = (request, response, next) => {
+  console.log('Request URL:', request.url);
+  next();
+};
+
+const timeLogger = (request, response, next) => {
+  console.log('Datetime:', new Date(Date.now()).toString());
+  next();
+};
+
+
+app.use('/sunsets', express.static(path.join(__dirname, 'public/images')))
+app.use(urlLogger, timeLogger);
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (request, response) => {
+  response.send('hello world');
+});
+
+app.get('/json', (request, response) => {
+  response.status(200).json({"name": "Cole"});
+});
+
+app.get('/sunsets', (req, res) => {
+  res.status(200).send('hello')
+})
+
+app.listen(3000, () => {
+  console.log('Express intro running on localhost:3000');
+});
